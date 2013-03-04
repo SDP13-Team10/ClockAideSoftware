ee4fa832799b782664674391abf9d28d9dbf3549
Sachin Honnudike <shonnudi@student.umass.edu>
2013-03-03 22:01:45 -0500

    Updated VoiceMaps

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
-import string,time,datetime,serial,re,subprocess
+import string,time,datetime,serial,re,subprocess,os
 
-keypad = None #Keypad + LCD display connected to the keypad
-motors = None #Stepper motors + LCD dispaly connected to the arduino
+#keypad = None #Keypad + LCD display connected to the keypad
+#motors = None #Stepper motors + LCD dispaly connected to the arduino
 
 def main():
-	print('Hello!')
-	getTimeString()
+        initializeHardware()
+        speakTime(005,045)
 
-#Initialize the two Arduino boards 
+#Initialize the two Arduino boards
 def initializeHardware():
 
-	#Parameters 
-	keypadBaudRate = 9600
-	motorBaudRate = 9600
-	keypadDevice = "/dev/ttyACM2"
-	motorDevice = "/dev/ttyACM0"
+        #Parameters
+        keypadBaudRate = 9600
+        motorBaudRate = 9600
+        keypadDevice = "/dev/ttyACM1"
+        motorDevice = "/dev/ttyACM0"
 
-	#Create Serial Object
-	global keypad = Serial.serial(keypadDevice, keypadBaudRate)
-	global motors = Serial.serial(motorDevice, motorBaudRate)
+        #Create Serial Object
+        #keypad = serial.Serial(keypadDevice, keypadBaudRate)
+        #motors = serial.Serial(motorDevice, motorBaudRate)
 
 #Return the complete time and date in string format
 #Required to initialize the stepper motors
 def getDateTimeString():
-	currentTime = datetime.datetime.now()
-	#print(currentTime.strftime("%I%M"))
-	return currentTime.strftime("%H, %M, %S, %d, %m, %Y")
+        currentTime = datetime.datetime.now()
+        #print(currentTime.strftime("%I%M"))
+        return currentTime.strftime("%H, %M, %S, %d, %m, %Y")
 
 #Returns the hour and minute in string format
 def getTimeString():
-	currentTime = datetime.datetime.now()
-	#print(currentTime.strftime("%I%M"))
-	return currentTime.strftime("%H, %M")
+        currentTime = datetime.datetime.now()
+        #print(currentTime.strftime("%I%M"))
+        return currentTime.strftime("%H, %M")
+
+def speakTime(hour, minute):
+
+        hourFile = "/home/pi/ClockAideSoftware/VoiceMap/Hours/"+str(hour)+".wav"
+
+        if minute == 0:
+                minuteFile="/home/pi/ClockAideSoftware/VoiceMap/Wildcards/oclock.wav"
+        else:
+                minuteFile="/home/pi/ClockAideSoftware/VoiceMap/Minutes/"+str(minute)+".wav"
+
+        playVoiceMap = "mplayer %s 1>/dev/null 2>&1 " + hourFile + " " + minuteFile
+        print(playVoiceMap)
+        os.system(playVoiceMap)
 
 if  __name__ =='__main__':main()
